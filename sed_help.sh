290cad5adf531b58bbf1cd16585bff79b49f5c69
Faust <faust@titane.home>
2020-09-17 22:32:22 +0200

    20200917 22h32

--- sed_help.sh
+++ sed_help.sh
@@ -1,3 +1,15 @@
 #/bin/sh 
-echo "----------"
+echo "--------------------------------------------------------------------------------"
 echo sed -i 's/old-text/new-text/g' input.txt
+echo "--------------------------------------------------------------------------------"
+
+ARGV1=$1
+ARGV1=$2
+DATE_JOUR=`date "+%Y%m%d-%Hh%M"`
+FILE_LIST=`grep -Grsl $1 *`
+echo $FILE_LIST
+
+for FILE in $FILE_LIST 
+do 
+    echo "->" $FILE " " $DATE_JOUR
+done
