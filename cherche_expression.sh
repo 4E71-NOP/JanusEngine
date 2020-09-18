7f27e837f42850e4f9fcd02f950eb45f995ef809
Faust <faust@titane.home>
2020-09-18 13:04:10 +0200

    20200918 13h04

--- cherche_expression.sh
+++ cherche_expression.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 clear
-ARGV1=$1
-LISTECIBLES=(modules version_actuelle websites-data)
+# ARGV1=$1
+TARGETS=(extensions modules current websites-data)
 
-for REPERTOIRE in "${LISTECIBLES[@]}"
+for T in "${TARGETS[@]}"
 do
-	echo -e "\n\e[46m\e[1mRecherche:"$ARGV1" dans "$REPERTOIRE"\e[21m\e[49m\e[39m"
-	grep -Ganrsl $ARGV1 --exclude-dir="vrs_20*" --exclude-dir="__help__ aide__ayuda__" --exclude-dir="graph" --exclude-dir="download" --exclude="*.jpg" --exclude="*.gif" --exclude="*.png" * | grep $REPERTOIRE"/"
+	echo -e "\n\e[46m\e[1mRecherche:\`"$1"\` dans "$T"\e[21m\e[49m\e[39m\e[m"
+	grep -Grsl $1 \
+		--exclude="*.jpg" \
+		--exclude="*.gif" \
+		--exclude="*.png" \
+		$T
 done
 
 DATE=`date`
 echo -e "\n\e[37m"$DATE
-echo -e "fin\e[49m\e[39m"
+echo -e "fin\e[49m\e[39m\e[m"
 
+#		--exclude-dir="vrs_2*" &&
+#		--exclude-dir="__help__ aide__ayuda__" &&
+#		--exclude="DeveloperDocs" &&
+#		--exclude="extensions" &&
+#		--exclude-dir="graph" &&
+#		--exclude-dir="download" &&
+# 		* | grep $T"/"
