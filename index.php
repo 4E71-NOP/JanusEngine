3326cff2b0ba792ee79e7df0c04b74ebe88d27d3
Faust <faust@rootwave.net>
2024-01-08 22:34:36 +0100

    fix session management

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-session_name ( "HydrWebsiteSessionId" );
-session_start ();
 /* Hydre-licence-debut */
 // --------------------------------------------------------------------------------------------
 //
@@ -13,6 +11,20 @@ session_start ();
 //
 // --------------------------------------------------------------------------------------------
 /* Hydre-licence-fin */
+
+if(session_status() !== PHP_SESSION_ACTIVE || session_status() === PHP_SESSION_NONE ) {
+	if ( session_start() === false ){
+		error_log( "*** WARNING *** session_start() returned false. Something went wrong and it's not a good start.");
+		session_unset();
+		session_destroy();
+		session_write_close();
+		session_name ( "HydrWebsiteSessionId" );
+		setcookie(session_name(), '', ['expires' => 0, 'path' => '/']);
+		session_regenerate_id(true);
+		session_start();
+		error_log( "*** WARNING *** Session status (_DISABLED = 0, _NONE = 1, _ACTIVE = 2) => " . session_status() );
+	}
+}
 
 include ("current/engine/utility/Hydr.php");
 $R = Hydr::getInstance();
