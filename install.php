e1cd42e88b36f16ff8430bd0fc08c20726bf15c0
Faust <faust@rootwave.net>
2024-01-09 15:10:03 +0100

    fix session (install & monitor)

--- install.php
+++ install.php
@@ -1,6 +1,31 @@
 <?php
-session_name ( "HydrWebsiteSessionId" );
-session_start ();
+/* Hydre-licence-debut */
+// --------------------------------------------------------------------------------------------
+//
+// Hydre - Le petit moteur de web
+// Sous licence Creative Common
+// Under Creative Common licence CC-by-nc-sa (http://creativecommons.org)
+// CC by = Attribution; CC NC = Non commercial; CC SA = Share Alike
+//
+// (c)Faust MARIA DE AREVALO faust@rootwave.net
+//
+// --------------------------------------------------------------------------------------------
+/* Hydre-licence-fin */
+
+if(session_status() !== PHP_SESSION_ACTIVE || session_status() === PHP_SESSION_NONE ) {
+	if ( session_start() === false ){
+		error_log( "*** WARNING *** session_start() returned false. Something went wrong and it's not a good start.");
+		session_unset();
+		session_destroy();
+		session_write_close();
+		session_name ( "HydrWebsiteSessionId" );
+		setcookie(session_name(), '', ['expires' => 0, 'path' => '/']);
+		session_regenerate_id(true);
+		session_start();
+		error_log( "*** WARNING *** Session status (_DISABLED = 0, _NONE = 1, _ACTIVE = 2) => " . session_status() );
+	}
+}
+
 include_once ("current/install/install_init.php");
 $R = HydrInstall::getInstance ();
 echo ($R->render ());
